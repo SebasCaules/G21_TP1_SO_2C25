08790054ddf0298defcabea4a2b00a708e325962
fedekloberdanz <fedeklober@gmail.com>
2025-04-14 22:38:51 +0000

    Break cuando se detecta que se termino el juego por timeout para que no siga iterando por los players y haya errores que causaban que freezee

--- master.c
+++ master.c
@@ -155,7 +155,7 @@ int main(int argc, char *argv[]) {
             time_t now = time(NULL);
             if (difftime(now, lastValidMove) >= timeout) {
                 state->hasFinished = true;
-                
+                break;
             }
             usleep(delay * 1000);
         }
