3006f66d0e3d41b32f1480c9c7ca016d344c4809
fedekloberdanz <fedeklober@gmail.com>
2025-04-14 15:54:47 +0000

    Comentario en nueva lib para uso gratis de PVS-Studio

--- validateLib.c
+++ validateLib.c
@@ -1,3 +1,5 @@
+// This is a personal academic project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: https://pvs-studio.com
 #include "validateLib.h"
 
 int dx[] = {  0,  1,  1,  1,  0, -1, -1, -1 };
